8a2c5a319be5b01c89d18997ad1c839c9ba4ee79
sakibdevlekar <sakibdevlekar96@gmail.com>
2024-04-22 22:19:14 +0530

    dependabot added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const { rl } = require("./utils/readLineInterFace");
 const { initializeProject } = require("./utils/initializeProject");
 
-// Prompt user for folder name
+/**
+ * Prompts the user for a project name and initializes a new project with the given name.
+ * @param {string} folderName - The name of the project folder.
+ */
 rl.question("Enter your project name: ", (folderName) => {
     initializeProject(folderName);
     rl.close();
--- src/utils/readLineInterFace.js
+++ src/utils/readLineInterFace.js
@@ -1,6 +1,8 @@
 const readline = require("readline");
 
 exports.rl = readline.createInterface({
+    // eslint-disable-next-line no-undef
     input: process.stdin,
+    // eslint-disable-next-line no-undef
     output: process.stdout,
 });
